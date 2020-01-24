55a1920fd637267c4b344825906abbba2a80dcab
Matthew Collier <mcollier@digitalbazaar.com>
2020-01-24 12:08:40 -0500

    Implement revocation API.

---
+++ test/test.config.js
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {config} = require('bedrock');
+const path = require('path');
+
+config.mocha.tests.push(path.join(__dirname, 'mocha'));
+
+// mongodb config
+config.mongodb.name = 'bedrock_zcap_storage_test';
+config.mongodb.host = 'localhost';
+config.mongodb.port = 27017;
+// drop all collections on initialization
+config.mongodb.dropCollections = {};
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
